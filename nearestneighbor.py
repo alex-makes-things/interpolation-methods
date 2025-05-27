a0a0411d13ad7d1190b2a9befef969e005d25f49
alex-makes-things <th3redshad0w@gmail.com>
2025-05-28 01:03:15 +0200

    Added comments

--- nearestneighbor.py
+++ nearestneighbor.py
@@ -6,19 +6,19 @@ im = Image.open('minilandscape.png')
 
 width, height = im.size
 pixels = im.load()
-scaling_factor = 1.5
+scaling_factor = 1.5  #can be any integer or decimal even between 0 and 1
 
 newpixels = []
-for b in range(floor(height*scaling_factor)):
-    for i in range(floor(width*scaling_factor)):
-        scaled_h = floor(b/scaling_factor)
+for b in range(floor(height*scaling_factor)):   #move vertically
+    for i in range(floor(width*scaling_factor)):    #append horizontally
+        scaled_h = floor(b/scaling_factor)    #map scaled values to original image
         scaled_w = floor(i/scaling_factor)
         newpixels.append((pixels[scaled_w, scaled_h][0], pixels[scaled_w, scaled_h][1], pixels[scaled_w, scaled_h][2], pixels[scaled_w, scaled_h][3]))
 
 
-newimg = np.array(newpixels, dtype=np.uint8)
+newimg = np.array(newpixels, dtype=np.uint8)   #dtype is crucial, data types are very important
 
-newimg= newimg.reshape((floor(height*scaling_factor),floor(width*scaling_factor),4))
+newimg= newimg.reshape((floor(height*scaling_factor),floor(width*scaling_factor),4))   #reshape the array into an image
 print(newimg)
 final = Image.fromarray(newimg, mode='RGBA')
 final.save("scaled.png")
